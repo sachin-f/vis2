44888f9dc1bee72c2945e39bdd142ea9a23e1e10
Sachin <sachin_f@yahoo.com.au>
2020-10-25 18:16:13 +1100

    updated data

--- data_wrangler.R
+++ data_wrangler.R
@@ -8,7 +8,15 @@ avg_pre_covid <- df_pre_covid %>%
   group_by(Airport, Month) %>%
   summarise_each(mean, Dom_Pax_In, Dom_Pax_Out, Int_Pax_In, Int_Pax_Out)
 
+avg_post_covid <- df %>%
+  filter(Year == 2020) %>%
+  select(Airport, Month, Dom_Pax_In, Dom_Pax_Out, Int_Pax_In, Int_Pax_Out)
+
 test <- df %>%
   filter(Airport=="ADELAIDE", Year != 2020, Month == 1)
   
 mean(test$Dom_Pax_In)
+
+write.csv(avg_pre_covid, "C:\\Users\\Orson\\Desktop\\DataVis\\Assignment 2\\vis2\\data\\airport_pre_avg.csv", row.names = FALSE)
+
+write.csv(avg_post_covid, "C:\\Users\\Orson\\Desktop\\DataVis\\Assignment 2\\vis2\\data\\airport_post_avg.csv", row.names = FALSE)
