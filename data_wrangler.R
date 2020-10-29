c99dc016829fa1afee5fdfa54622ca919cef9901
Sachin <sachin_f@yahoo.com.au>
2020-10-30 00:04:21 +1100

    Updated HTML/CSS

--- data_wrangler.R
+++ data_wrangler.R
@@ -1,4 +1,6 @@
-df <- read.csv("data/flight_data.csv") %>%
+library(tidyverse)
+
+df <- read.csv("data/new_data.csv") %>%
   rename(Airport = ï..Airport)
 
 df_pre_covid <- df %>%
@@ -6,11 +8,11 @@ df_pre_covid <- df %>%
 
 avg_pre_covid <- df_pre_covid %>%
   group_by(Airport, Month) %>%
-  summarise_each(mean, Dom_Pax_In, Dom_Pax_Out, Int_Pax_In, Int_Pax_Out)
+  summarise_each(mean, Dom_Pax_In, Dom_Pax_Out, Int_Pax_In, Int_Pax_Out, Pax_In, Pax_In)
 
 avg_post_covid <- df %>%
   filter(Year == 2020) %>%
-  select(Airport, Month, Dom_Pax_In, Dom_Pax_Out, Int_Pax_In, Int_Pax_Out)
+  select(Airport, Month, Dom_Pax_In, Dom_Pax_Out, Int_Pax_In, Int_Pax_Out, Pax_In, Pax_In)
 
 test <- df %>%
   filter(Airport=="ADELAIDE", Year != 2020, Month == 1)
